a237586771bbc7c6454f2822350dcfe3d34ad70c
Chunzhi(Hannah) Zhou <zhouchunzhi@hannah-zhou.bc.edu>
2018-02-01 15:11:13 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by 周纯稚 on 01/02/2018.
+//  Copyright © 2018 周纯稚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
